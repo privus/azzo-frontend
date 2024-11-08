9863ea8c8973d95cfcac81a0becb730578383641
andrejvb <andre.jvb@gmail.com>
2024-11-08 10:27:00 -0300

    Volta para versão desenvolvimento local

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
   appVersion: 'v8.2.4',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: true,
-  apiUrl: 'http://52.67.57.159',
+  apiUrl: 'localhost:3000',
   appThemeName: 'Metronic',
   appPurchaseUrl: 'https://1.envato.market/EA4JP',
   appHTMLIntegration: 'https://preview.keenthemes.com/metronic8/demo1/documentation/base/helpers/flex-layouts.html',
