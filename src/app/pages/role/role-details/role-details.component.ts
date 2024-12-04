1e6ac7dd1b4cd25e1295411fa916f3285a633c29
andrejvb <andre.jvb@gmail.com>
2024-12-04 15:47:09 -0300

    ajusta service, role listing, details e model

--- src/app/pages/role/role-details/role-details.component.ts
+++ src/app/pages/role/role-details/role-details.component.ts
@@ -1,10 +1,7 @@
-// role-details.component.ts
 import { ChangeDetectorRef, Component, EventEmitter, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Cargo, Permissao, UserUpdate, Usuario } from '../../../modules/account/models/user.model';
+import { Cargo, Usuario } from '../../../modules/account/models/user.model';
 import { AzzoService } from '../../../core/services/azzo.service';
-import { PERMISOES } from '../../../shared/constants/user-constant';
-import { Config } from 'datatables.net';
 import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
 import { SweetAlertOptions } from 'sweetalert2';
 import { AccountService } from '../../../modules/account/services/account.service';
@@ -16,10 +13,7 @@ import { AccountService } from '../../../modules/account/services/account.servic
 })
 export class RoleDetailsComponent implements OnInit {
   role: Cargo;
-  permissionsList: Permissao[] = PERMISOES;
-  rolePermissions: Permissao[] = [];
   users: Usuario[] = [];
-  datatableConfig: Config = {};
   reloadEvent: EventEmitter<boolean> = new EventEmitter();
   @ViewChild('noticeSwal') noticeSwal!: SwalComponent;
   swalOptions: SweetAlertOptions = {};
@@ -38,21 +32,27 @@ export class RoleDetailsComponent implements OnInit {
       return;
     }
 
+    this.loadRoleDetails(roleId);
+    this.loadUsersByRole(roleId);
+  }
+
+  private loadRoleDetails(roleId: number): void {
     this.azzoService.getRoleById(roleId).subscribe({
       next: (roleData) => {
         this.role = roleData;
-        this.rolePermissions = this.getPermissionsFromSoma(roleData.somaPermissao);
         this.cdr.detectChanges();
       },
       error: (error) => {
         console.error('Erro ao buscar os dados da role:', error);
       },
     });
+  }
+
+  private loadUsersByRole(roleId: number): void {
     this.azzoService.getUsersByRole(roleId).subscribe({
       next: (usersData) => {
         this.users = usersData;
         this.cdr.detectChanges();
-        console.log('Usuários da role:', usersData);
       },
       error: (error) => {
         console.error('Erro ao buscar os usuários da role:', error);
@@ -60,45 +60,31 @@ export class RoleDetailsComponent implements OnInit {
     });
   }
 
-  // Method to get permissions from somaPermissao
-  getPermissionsFromSoma(somaPermissao: number): Permissao[] {
-    return this.permissionsList.filter((p) => (somaPermissao & p.permissao) !== 0);
-  }
-
-  getRandomColor(): string {
-    const colors = ['success', 'info', 'warning', 'danger'];
-    return colors[Math.floor(Math.random() * colors.length)];
-  }
-
   removeRole(user: Usuario): void {
-    const updatedUser: Partial<UserUpdate> = { cargo_id: null };
+    const updatedUser = { cargo_id: null };
 
     this.accountService.updateUserInfo(user.usuario_id, updatedUser).subscribe({
-      complete: () => {
-        // Atualiza a lista de usuários local após a remoção
+      next: () => {
         this.users = this.users.filter((u) => u.usuario_id !== user.usuario_id);
         this.showAlert({
           icon: 'success',
           title: 'Removido!',
           text: `O usuário ${user.nome} foi removido com sucesso.`,
-          showCancelButton: false,
-          confirmButtonText: 'Ok',
         });
         this.cdr.detectChanges();
       },
       error: (error) => {
         console.error('Erro ao remover o cargo do usuário:', error);
-        this.swalOptions = {
+        this.showAlert({
           icon: 'error',
           title: 'Erro!',
           text: 'Não foi possível remover o usuário do cargo.',
-        };
-        this.noticeSwal.fire();
+        });
       },
     });
   }
 
-  showAlert(swalOptions: SweetAlertOptions) {
+  showAlert(swalOptions: SweetAlertOptions): void {
     this.swalOptions = swalOptions;
     this.cdr.detectChanges();
     this.noticeSwal.fire();
--- src/app/shared/constants/user-constant.ts
+++ src/app/shared/constants/user-constant.ts
@@ -1,4 +1,4 @@
-import { Permissao, Regiao } from '../../modules/account/models/user.model';
+import { Regiao } from '../../modules/account/models/user.model';
 
 export const REGIOES: Regiao[] = [
   { regiao_id: 1, nome: 'Norte' },
@@ -6,10 +6,4 @@ export const REGIOES: Regiao[] = [
   { regiao_id: 3, nome: 'Centro-Oeste' },
   { regiao_id: 4, nome: 'Sudeste' },
   { regiao_id: 5, nome: 'Sul' },
-];
-export const PERMISOES: Permissao[] = [
-  { permissao: 8, nome: 'Administrador' }, // 2^3
-  { permissao: 1, nome: 'Gerenciamento de Usuários' }, // 2^0
-  { permissao: 2, nome: 'Gerenciamento Financeiro' }, // 2^1
-  { permissao: 4, nome: 'Gerenciamento Estoque' }, // 2^2
 ];
