7b6cebdef0c07ccd26286ddd466fb736b41b6019
andrejvb <andre.jvb@gmail.com>
2025-01-22 14:43:35 -0300

    config para container e limpa footer

--- src/app/_metronic/layout/layout.module.ts
+++ src/app/_metronic/layout/layout.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { InlineSVGModule } from 'ng-inline-svg-2';
 import { RouterModule, Routes } from '@angular/router';
 import { NgbDropdownModule, NgbProgressbarModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
-import { TranslateModule } from '@ngx-translate/core';
 import { TranslationModule } from '../../modules/i18n';
 import { LayoutComponent } from './layout.component';
 import { ExtrasModule } from '../partials/layout/extras/extras.module';
@@ -16,13 +15,11 @@ import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { TopbarComponent } from './components/topbar/topbar.component';
 import { PageTitleComponent } from './components/header/page-title/page-title.component';
 import { HeaderMenuComponent } from './components/header/header-menu/header-menu.component';
-import { DrawersModule, DropdownMenusModule, ModalsModule, EngagesModule } from '../partials';
-import { EngagesComponent } from '../partials/layout/engages/engages.component';
+import { DropdownMenusModule, ModalsModule } from '../partials';
 import { ThemeModeModule } from '../partials/layout/theme-mode-switcher/theme-mode.module';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { SidebarLogoComponent } from './components/sidebar/sidebar-logo/sidebar-logo.component';
 import { SidebarMenuComponent } from './components/sidebar/sidebar-menu/sidebar-menu.component';
-import { SidebarFooterComponent } from './components/sidebar/sidebar-footer/sidebar-footer.component';
 import { NavbarComponent } from './components/header/navbar/navbar.component';
 import { AccountingComponent } from './components/toolbar/accounting/accounting.component';
 import { ClassicComponent } from './components/toolbar/classic/classic.component';
@@ -51,11 +48,9 @@ const routes: Routes = [
     TopbarComponent,
     PageTitleComponent,
     HeaderMenuComponent,
-    EngagesComponent,
     SidebarComponent,
     SidebarLogoComponent,
     SidebarMenuComponent,
-    SidebarFooterComponent,
     NavbarComponent,
     AccountingComponent,
     ClassicComponent,
@@ -72,11 +67,8 @@ const routes: Routes = [
     NgbProgressbarModule,
     ExtrasModule,
     ModalsModule,
-    DrawersModule,
-    EngagesModule,
     DropdownMenusModule,
     NgbTooltipModule,
-    TranslateModule,
     ThemeModeModule,
     SharedModule,
   ],
