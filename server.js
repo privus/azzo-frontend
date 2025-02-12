dacfb4bf03864d5f2adb85cf77a060036a12191b
andrejvb <andre.jvb@gmail.com>
2025-02-12 17:33:35 -0300

    config wordpane 7

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const path = require('path');
 
 const app = express();
 
-// Servindo arquivos estáticos corretamente
+// Caminho correto para os arquivos estáticos
 const publicPath = path.join(__dirname, 'public_html');
 app.use('/azzo', express.static(publicPath));
 
-// Corrige o caminho do index.html
+// Garantir que todas as rotas do Angular sejam redirecionadas para `index.html`
 app.get('/azzo/*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
+// Porta de execução
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Frontend rodando em http://localhost:${PORT}/azzo`);
