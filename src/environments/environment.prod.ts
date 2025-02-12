1d9fd67d31eb2622cb5fe234a5147b522cd62bab
andrejvb <andre.jvb@gmail.com>
2025-02-12 14:54:09 -0300

    config wordpane

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/',
+  apiUrl: 'https://api.geecom.com.br/api/',
   appThemeName: 'Azzo',
   appName: 'Azzo',
   appVersion: '1.0.0',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/',
+  apiUrl: 'https://api.geecom.com.br/api/',
   appName: 'Azzo',
   appThemeName: 'Azzo',
   appVersion: '1.0.0',
