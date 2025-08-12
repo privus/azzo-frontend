de6cd1b7cf1a799003ffc7b2cb064632912001a5
andrejvb <andre.jvb@gmail.com>
2025-08-12 15:37:27 -0300

    fix: ajuste filtro entre datas order-details4

--- src/app/modules/commerce/order-listing/order-listing.component.ts
+++ src/app/modules/commerce/order-listing/order-listing.component.ts
@@ -304,6 +304,8 @@ export class OrderListingComponent implements OnInit {
       if (end) {
         const toDate = this.formatDate(this.parseLocalDate(end));
 
+        console.log('front (component) -> from:', fromDate, 'to:', toDate);
+
         this.orderService.getOrdersBetweenDates(fromDate, toDate).subscribe({
           next: (orders) => {
             this.orders = orders;
